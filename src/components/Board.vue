8c365dbe8e3969372aceadf00684caa610d8af45
mhleeuwe <mhleeuwe@avans.nl>
2022-11-01 17:33:24 +0100

    init commit

---
+++ src/components/Board.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="flex">
+        <div class="grid grid-cols-8 w-auto">
+            <div class="w-16 h-16 border" :class="getSquareColorClass(n)" v-for="n in 64"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        getSquareColorClass(n) {
+            n = n - 1
+            var x = n % 2
+            var y = Math.floor(n / 8) % 2
+
+            if(x == 0 && y == 0) 
+            {
+                return "bg-white"
+            }
+            else if (x == 1 && y == 0)
+            {
+                return "bg-black"
+            }
+            else if(x == 0 && y == 1) 
+            {
+                return "bg-black"
+            }
+            else if (x == 1 && y == 1)
+            {
+                return "bg-white"
+            }
+        }
+    }
+}
+</script>
---
+++ src/views/BoardView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Board from '../components/Board.vue'
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is the board page</h1>
+
+    <Board />
+  </div>
+</template>
