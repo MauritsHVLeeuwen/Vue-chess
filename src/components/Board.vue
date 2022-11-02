fbfe2b97f313b7adaf1699fa00638416b7f29f76
mhleeuwe <mhleeuwe@avans.nl>
2022-11-02 17:15:21 +0100

    added king & queen

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,23 +1,14 @@
+<script setup>
+import Square from '../components/Square.vue'
+</script>
+
+
 <template>
     <div class="flex">
         <div class="grid grid-cols-8 w-auto">
             <div class="w-16 h-16 border" v-for="x in 8">
-                <div class="w-16 h-16 border" :class="getSquareColorClass(x, y)" v-for="y in 8"></div>
+                <Square v-for="y in 8" :x="x" :y="y" />
             </div>
         </div>
     </div>
 </template>
-
-<script>
-export default {
-    methods: {
-        getSquareColorClass(x, y) {
-            if(((x - y) % 2) == 0) 
-            {
-                return "bg-white"
-            }
-            return "bg-black"
-        }
-    }
-}
-</script>
---
+++ src/components/Square.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="w-16 h-16 border" :class="getSquareColorClass">
+        <black-king v-if="black_king" />
+        <black-queen v-if="black_queen" />
+    </div>
+</template>
+
+<script>
+import BlackKing from './BlackKing.vue';
+import BlackQueen from './BlackQueen.vue';
+
+export default {
+  components: { BlackKing, BlackQueen },  
+    props: ['x', 'y'],  
+    data: function() {
+        return {
+            black_king: false,
+            black_queen: false,
+        }
+    },
+    computed: {
+        getSquareColorClass() {
+            if(((this.y - this.x) % 2) == 0) 
+            {
+                return "bg-white"
+            }
+            return "bg-gray-500"
+        }
+    },
+    mounted: function() {
+        if(this.x == 5 && this.y == 1)
+        {
+            this.black_king = true;
+        }
+        if(this.x == 4 && this.y == 1)
+        {
+            this.black_queen = true;
+        }
+    }
+}
+</script>
