7421e636e9a0ac4952b65898505f960e52efa8df
mhleeuwe <mhleeuwe@avans.nl>
2022-11-04 16:54:43 +0100

    basic vuex

--- src/views/BoardView.vue
+++ src/views/BoardView.vue
@@ -1,29 +1,19 @@
-<script setup>
-import Board from '../components/Board.vue'
-import { createApp } from 'vue'
-import { createStore } from 'vuex'
-
-const store = createStore({
-  state () {
-    return {
-      count: 0
-    }
-  },
-  mutations: {
-    increment (state) {
-      state.count++
-    }
-  }
-})
-
-const app = createApp({})
-
-app.use(store)
-</script>
-
 <template>
   <div>
-    count is: {{store.state.count}}
+    count is: {{count}}
     <Board />
   </div>
 </template>
+
+<script>
+import Board from '../components/Board.vue'
+
+export default{
+  components: { Board },
+  computed: {
+    count() {
+      return this.$store.state.count
+    }
+  }
+}
+</script>
